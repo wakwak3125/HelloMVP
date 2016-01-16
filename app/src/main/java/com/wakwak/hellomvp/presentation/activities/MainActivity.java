b71cbf07123ed15e7bb5446f700821190344afb6
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2016-01-17 02:36:03 +0900

    added MainActivity and MainFragment

---
+++ app/src/main/java/com/wakwak/hellomvp/presentation/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wakwak.hellomvp.presentation.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wakwak.hellomvp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
